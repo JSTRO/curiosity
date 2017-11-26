f852881536fc4e3a32e5d6344761d0e8b76053b0
Joseph Ostrow <jstro@umich.edu>
2017-11-26 01:15:01 -0500

    add camera selector

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=yXagGhEcvXJrTEFKzLFx2iJYPAfUI6bwgLy8JuI8"
+var baseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&";
+var apiKey = "api_key=yXagGhEcvXJrTEFKzLFx2iJYPAfUI6bwgLy8JuI8";
+var cameraList = ["FHAZ", "RHAZ", "MAST", "CHEMCAM", "MAHLI", "NAVCAM"];
+var camera;
+var url;
 
-$.getJSON(url, function(data) {
+$("button.dropdown-item").click(function() {
+  camera = cameraList[this.id];
+  url = baseUrl + "camera=" + camera + "&" + apiKey;
+  var dropdownTitle = $("button.dropdown-item#" + this.id).text();
+  $(".dropdown-toggle").html(dropdownTitle);
 
-  var items = [];
-  for (i=0; i<data.photos.length; i++) {
-    items.push("<li id='" + data.photos[i].id + "'>" + "<img src='" + data.photos[i].img_src + "'>" + "</li>" );
-  }
+  $(".images").empty();
 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo("div");
+  $.getJSON(url, function(data) {
+    var items = [];
+    for (i=0; i<data.photos.length; i++) {
+      items.push("<div class='row' id='" + data.photos[i].id + "'>" + 
+                   "<div class='col-md-6'><img src='" + 
+                   data.photos[i].img_src + "'></div>" + 
+                 "</div" );
+    }
+
+    $( "<div/>", {
+      "class": "container-fluid", 
+      html: items.join( "" )
+    }).appendTo(".images");
+
+  });        
 });
+
+ 
+
+
+
